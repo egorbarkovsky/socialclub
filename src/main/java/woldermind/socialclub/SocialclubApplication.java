85d73042698e9aa562cb0246058664913fc8b70a
Egor Barkovsky <woldermind@mail.ru>
2020-03-07 00:22:11 +0300

    Init

---
+++ src/main/java/woldermind/socialclub/SocialclubApplication.java
@@ -0,0 +1,13 @@
+package woldermind.socialclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/woldermind/socialclub/SocialclubApplicationTests.java
@@ -0,0 +1,13 @@
+package woldermind.socialclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
